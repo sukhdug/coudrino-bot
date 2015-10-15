ca07a0bcb99281e46b20392e9b25838e1781099d
Davide Pedranz <davide.pedranz@gmail.com>
2015-10-15 16:35:47 +0200

    Refactor bot spec

--- test/bot-spec.js
+++ test/bot-spec.js
@@ -12,11 +12,6 @@ var TelegramBot = require('node-telegram-bot-api');
 var Bluebird = require('bluebird');
 var extend = require('util')._extend;
 
-// helper function to run the checks later
-var later = function (fc) {
-    setTimeout(fc, 500);
-};
-
 // constants
 var CHAT_ID = 11111;
 var ME = Object.freeze({
@@ -43,14 +38,26 @@ var MESSAGE_OBJ = Object.freeze({
     text: '/start'
 });
 
+
 /**
- * Simulate a new incoming message
+ * Simulate a new incoming message.
  */
 var simulateMessage = function (bot, command) {
     var msg = extend({}, MESSAGE_OBJ);
     msg.text = command;
     bot.emit('text', msg);
 };
+
+/**
+ * Verify a mock object after a certain amount of time.
+ */
+var check = function (mock, done) {
+    setTimeout(function () {
+        mock.verify();
+        done();
+    }, 500);
+};
+
 
 // inject a fake TOKEN for the bot
 process.env.TOKEN = 'token';
@@ -65,19 +72,18 @@ sinon.stub(TelegramBot.prototype, 'getMe')
 var bot = require('../src/bot');
 
 
+// spec
 describe('bot', function () {
 
-    // setup sinon
     beforeEach(function (done) {
-        this.sinon = sinon.sandbox.create();
 
         // clear the db before the start of the test
         bot.reset().then(function () {
             done();
         });
-    });
-    afterEach(function () {
-        this.sinon.restore();
+
+        // mock the bot
+        this.mock = sinon.mock(bot);
     });
 
     describe('#getMe() [MOCK]', function () {
@@ -91,80 +97,49 @@ describe('bot', function () {
 
     describe('/command@Bot', function () {
         it('it should execute the command without @Bot if it refers to this bot', function (done) {
-            var sendMessage = this.sinon.spy(bot, 'sendMessage');
+            this.mock.expects('sendMessage').once().withArgs(CHAT_ID, Messages.WELCOME);
             simulateMessage(bot, '/start@' + ME.username);
-
-            later(function () {
-                assert.calledOnce(sendMessage);
-                assert.calledWith(sendMessage, CHAT_ID, Messages.WELCOME);
-                done();
-            });
+            check(this.mock, done);
         });
         it('it should do nothing if the command does not refer to this bot', function (done) {
-            var sendMessage = this.sinon.spy(bot, 'sendMessage');
+            this.mock.expects('sendMessage').never();
             simulateMessage(bot, '/start@NotMe');
-
-            later(function () {
-                assert.notCalled(sendMessage);
-                done();
-            });
+            check(this.mock, done);
         });
     });
 
     describe('/start', function () {
         it('it should send a welcome message', function (done) {
-            var sendMessage = this.sinon.spy(bot, 'sendMessage');
+            this.mock.expects('sendMessage').once().withArgs(CHAT_ID, Messages.WELCOME);
             simulateMessage(bot, '/start');
-
-            later(function () {
-                assert.calledOnce(sendMessage);
-                assert.calledWith(sendMessage, CHAT_ID, Messages.WELCOME);
-                done();
-            });
+            check(this.mock, done);
         });
     });
 
     describe('/cancel', function () {
         it('should do nothing if there is no active command', function (done) {
-            var sendMessage = this.sinon.spy(bot, 'sendMessage');
+            this.mock.expects('sendMessage').once().withArgs(CHAT_ID, Messages.NO_ACTIVE_COMMAND);
             simulateMessage(bot, '/cancel');
-
-            later(function () {
-                assert.calledOnce(sendMessage);
-                assert.calledWith(sendMessage, CHAT_ID, Messages.NO_ACTIVE_COMMAND);
-                done();
-            });
+            check(this.mock, done);
         });
-
         it('should cancel the active command', function (done) {
-            var self = this;
 
             // old state -> add email
+            this.mock.expects('sendMessage').once();
             simulateMessage(bot, '/add');
 
-            later(function () {
-                var sendMessage = self.sinon.spy(bot, 'sendMessage');
-                simulateMessage(bot, '/cancel');
-
-                later(function () {
-                    assert.calledOnce(sendMessage);
-                    assert.calledWith(sendMessage, CHAT_ID, Messages.COMMAND_CANCELLED);
-                    done();
-                });
-            });
+            // cancel the active command
+            this.mock.expects('sendMessage').once().withArgs(CHAT_ID, Messages.COMMAND_CANCELLED);
+            simulateMessage(bot, '/cancel');
+            check(this.mock, done);
         });
     });
 
     describe('default', function () {
         it('should return a help message if no command is found', function (done) {
-            var sendMessage = this.sinon.spy(bot, 'sendMessage');
+            this.mock.expects('sendMessage').once().withArgs(CHAT_ID, Messages.UNKNOWN_COMMAND);
             simulateMessage(bot, '/nonExistingCommand');
-
-            later(function () {
-                assert.calledOnce(sendMessage);
-                assert.calledWith(sendMessage, CHAT_ID, Messages.UNKNOWN_COMMAND);
-                done();
-            });
+            check(this.mock, done);
         });
     });
 
